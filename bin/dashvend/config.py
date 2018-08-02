dda080c58408fae7ad180e814508df323474171e
zSAM-Project <40679306+zSAM-Project@users.noreply.github.com>
2018-08-02 13:24:30 +0700

    Update config.py

--- bin/dashvend/config.py
+++ bin/dashvend/config.py
@@ -1,5 +1,5 @@
 # set this to the full path to the dashvend respository checkout
-DASHVEND_DIR = '/home/pi/dashvend'
+DASHVEND_DIR = '/home/stampcoin/dashvend'
 # note: also update paths in:
 #   bin/.init.d.dashvend
 #   bin/_dashvend_control.sh
@@ -7,10 +7,10 @@ DASHVEND_DIR = '/home/pi/dashvend'
 #   bin/show_screen_number.sh
 
 # after testing, set this to True to use mainnet
-MAINNET = False
+MAINNET = True
 
 # **public** mainnet bip32 seed for vending address generation
-BIP32_MAINNET_SEED = 'drkpRxPP5eefb7GqpdDSQKGPjJHHuBqPJJVD2Qx4BHF7CVP1dC8uVxVy6JfDQsn1U1EazDZPa4DWMsmV7pDhMtLTQQypHHc6cFnPPYZvwib5nVi'  # noqa
+BIP32_MAINNET_SEED = 'ToEA6jDNwU51aWKVEp7t42nG8FhmUjHdDfayFp5eR88jnLJMEe2GjVMBc6TxbewEALgUMT1FDj7AQYpxqQq5WoQnhwsZehj23cdy9pBXY5SxR5m'  # noqa
 # on a secure machine, generate above with pycoin 'ku' command.
 # for testing, ku is already installed on this machine during the 'make'
 # install pycoin by doing:
@@ -21,7 +21,7 @@ BIP32_MAINNET_SEED = 'drkpRxPP5eefb7GqpdDSQKGPjJHHuBqPJJVD2Qx4BHF7CVP1dC8uVxVy6J
 # for sane passphrase selection see: https://masternode.me/smart_passwords.html
 
 # **public** testnet bip32 seed for vending address generation
-BIP32_TESTNET_SEED = 'DRKPuUbaZSQn2SV5vyTh9DRHcooktYP3TB3NQa8cgMGXxT8znzH5opFtDgY8PVTKzTohyEfitf1TkcxnygJdY7ACJxvbVTvSVn6q6gCEVfydtJ6y'  # noqa
+BIP32_TESTNET_SEED = 'DRKPuUbMa5buKiQRRCQggBzHV2DHbDLjhFFRdHCRHjvx4jq1BCC1NZ1gDoi3ybD5NPHQfHWuxLXrRNQfyUU74YVhAx9kTmt3X87mjGaHiwGGqoad'  # noqa
 # ku -n tDASH -s 0p/0 P:<a unique, unpublished 100 character long sentence>
 
 # require seven (out of ten) masternode locks to accept purchase
@@ -30,4 +30,4 @@ IX_LOCK_TRESHOLD = 7
 # settings above 9 are not recommended.
 
 # dash value required to trigger sale
-VENDING_COST = 0.01
+VENDING_COST = 0.1
